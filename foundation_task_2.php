ea0286e1fcc56c3bf093c3bc49d67bfa5ab9c4f1
Tyrique de Bruin <Tyriquedebruin@gmail.com>
2024-02-23 15:33:47 +0200

    Program that will start at 0 and output the fibonacciSequence up to 34

--- foundation_task_2.php
+++ foundation_task_2.php
@@ -1,3 +1,14 @@
 <?php
+    function fibonacciSequence($IntNumOfElements, $IntFirstNum = 0, $IntSecondNum = 1) {
+        if($IntNumOfElements == 0) {
+            return;
+        }
+        echo $IntFirstNum.", ";
+        $IntNextNum = $IntFirstNum + $IntSecondNum;
+        fibonacciSequence($IntNumOfElements -1, $IntSecondNum, $IntNextNum);
+    }
 
+    $IntNumOfElements = 10;
+    echo "Fibonacci series for ".$IntNumOfElements." elements: ";
+    fibonacciSequence($IntNumOfElements);
 ?>
