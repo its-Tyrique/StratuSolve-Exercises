cdcd46fe0e83f09100127c59094e88967f6164dd
Tyrique de Bruin <Tyriquedebruin@gmail.com>
2024-02-23 15:50:51 +0200

    Created foundation_task_3.php

---
+++ foundation_task_3.php
@@ -0,0 +1,14 @@
+<?php
+    class Palindrome {
+        public static function isPalindrome($word) {
+        //TODO: Implement this
+            return false;
+        }
+    }
+
+    if (Palindrome::isPalindrome('Never Odd Or Even'))
+        echo 'Palindrome';
+    else
+        echo 'Not palindrome';
+
+?>
