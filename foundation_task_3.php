57e7159cf364c815620a564f867c11686933d86f
tyrique <tyriquedebruin@gmail.com>
2024-03-01 01:02:28 +0200

    Foundation Task - Added Palindrome class with non-static method for Fibonacci series https://trello.com/c/uZoQq6Ce

--- foundation_task_3.php
+++ foundation_task_3.php
@@ -1,4 +1,5 @@
 <?php
+//  Palindrome class with static method
     class Palindrome {
         public static function isPalindrome($word) {
 
@@ -17,3 +18,24 @@
     } else {
         echo 'Not palindrome';
     }
+
+//    Palindrome class with non-static method
+    /*class Palindrome {
+        public function isPalindrome($word) {
+            $word = preg_replace('/[^a-z0-9]/', '', strtolower($word));
+
+            if ($word == strrev($word)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    $palindromeChecker = new Palindrome();
+
+    if ($palindromeChecker->isPalindrome('Never Odd Or Even')) {
+        echo 'Palindrome';
+    } else {
+        echo 'Not palindrome';
+    }*/
